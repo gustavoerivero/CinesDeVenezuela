c153fb5fd5a9535b0fe2c63ccae1e0031f6e9030
Your Name <danykaram97@gmail.com>
2020-06-22 19:50:49 -0400

    test

--- src/controllers/ControllerMainMenu.java
+++ src/controllers/ControllerMainMenu.java
@@ -33,7 +33,7 @@ public class ControllerMainMenu implements ActionListener, MouseListener, ItemLi
                 
         // Models
         private Invoice invM;
-    
+        //Holaaaa
             // Models.DataBase
             private CandyCRUD can;
             private ClientCRUD cli;
